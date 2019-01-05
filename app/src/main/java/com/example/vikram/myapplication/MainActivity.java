509e87887c89496c0f57a93c169c1142995fdb9b
vikramsingh1997 <llearning948@gmail.com>
2019-01-05 20:32:30 +0530

    chnage with variable in th main activity class

--- app/src/main/java/com/example/vikram/myapplication/MainActivity.java
+++ app/src/main/java/com/example/vikram/myapplication/MainActivity.java
@@ -4,13 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private  int h;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    void getData()
-    {
+
+    void getData() {
         System.out.print("hello");
     }
 }
